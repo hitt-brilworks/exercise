4db7d646a49015441f10d3543051cbc0c30b1b59
hitt-brilworks <hit.t@brilworks.com>
2024-08-20 16:12:06 +0530

    completed exercise 7

---
+++ exercise_7/ex7.js
@@ -0,0 +1,28 @@
+// Exercise:
+// ○ Declare variables of different types: string, number, boolean, null, undefined.
+// ○ Use typeof to check the type of each variable.
+// ○ Experiment with changing the type of a variable (e.g., from a number to a string).
+
+// primitive data types
+let a = "string"; // string
+let b = Symbol("name"); // symbol
+let c = 45; // number
+let d = null; // null
+let e = true; // boolean
+let f = 8758787598758974n; // bigint
+let g = undefined; // undefined
+
+// typeof to check the type of each variable.
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+
+//  Experiment with changing the type of a variable (e.g., from a number to a string).
+a = 5;
+b = true;
+console.log(typeof a);
+console.log(typeof b);
